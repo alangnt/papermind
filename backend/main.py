779c710a8b354f162a8a41c437b56f9e1d88db69
Alan Geirnaert <geirnaert.alan@icloud.com>
2025-07-13 23:11:54 +0200

    fix: added online domain to the CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # middleware
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost:3000"],
+	allow_origins=[
+		"http://localhost:3000",
+		"https://astro-ai-sigma.vercel.app"
+	],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
