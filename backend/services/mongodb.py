ee7d8a761b3ebe724b69134f86ed1b898a7d2d8a
Alan Geirnaert <geirnaert.alan@icloud.com>
2025-07-14 17:54:20 +0200

    fix: removed loadenv

--- backend/services/mongodb.py
+++ backend/services/mongodb.py
@@ -1,10 +1,5 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv # remove in production
-from pathlib import Path # remove in production
-
-env_path = Path(__file__).resolve().parent.parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)  # remove in production
 
 uri = os.getenv('MONGODB_URI')
 if not uri:
