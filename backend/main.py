c6839b871aac2686232ed99429f2d2d18905891b
Alan Geirnaert <geirnaert.alan@icloud.com>
2025-07-14 21:02:52 +0200

    fix: removed __main__

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,3 @@ async def root():
 app.include_router(get_documents.router)
 app.include_router(embed_documents.router)
 app.include_router(vector_search.router)
-
-if __name__ == "__main__":
-	uvicorn.run("main:app", host="0.0.0.0", port=8080)
