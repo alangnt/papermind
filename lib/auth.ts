fb170cf929dd92aa0941f376e982eea75a5decd1
alangnt <geirnaert.alan@icloud.com>
2026-05-03 20:38:40 +0200

    feat: env security

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,8 +3,16 @@ import { dash } from "@better-auth/infra";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
-const db = client.db();
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+if (!process.env.MONGODB_NAME) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_NAME"');
+}
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db(process.env.MONGODB_NAME);
 
 export const auth = betterAuth({
   database: mongodbAdapter(db),
